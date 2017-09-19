7d1517511e50234db680f7d7daf93e2302e406c2
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2017-09-18 18:17:25 -0700

    Use klgists Connection class. Might break backwards-compatibility.

--- valarpy/global_connection.py
+++ valarpy/global_connection.py
@@ -1,23 +1,5 @@
-import os
-import json
-import logging
-from typing import Dict, Union
-from valarpy.connection import Connection
+from klgists.db.connection import Connection
 
 db = None
 
-class GlobalConnection(Connection):
-
-	@classmethod
-	def from_json(cls, config_path: str):
-		if os.path.isfile(config_path) and os.access(config_path, os.R_OK):
-			logging.info("Using Valar connection from '{}'".format(config_path))
-			with open(config_path) as jscfg:
-				params = json.load(jscfg)  # type: Dict[str, Union[str, int, None]]
-				return cls(**params)
-		else:
-			raise ValueError("{} does not exist, is not a file, or is not readable".format(config_path))
-
-	@classmethod
-	def from_dict(cls, dct: Dict[str, str]):
-		return cls(**dct)
+GlobalConnection = Connection  # for backwards-compatibility
