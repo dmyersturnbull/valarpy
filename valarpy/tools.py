23de0c4330808602d30a1a2dc62a4b7b5926d214
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2018-01-12 21:03:18 -0800

    Moved some utils to klgists.

--- valarpy/tools.py
+++ valarpy/tools.py
@@ -7,34 +7,11 @@ import pandas as pd
 import peewee
 from hurry.filesize import size as _hurrysize
 
+from klgists.common import json_serial, pretty_dict, pp_dict
+
+class ValarLookupError(KeyError): pass
 
 class Tools:
-
-	@staticmethod
-	def json_serial(obj):
-		"""JSON serializer for objects not serializable by default json code.
-			From jgbarah at https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable
-			"""
-		if isinstance(obj, (datetime, date)):
-			return obj.isoformat()
-		if isinstance(obj, peewee.Field):
-			return type(obj).__name__
-		raise TypeError("Type %s not serializable" % type(obj))
-
-	@staticmethod
-	def pretty_dict(dct: dict) -> str:
-		"""Returns a pretty-printed dict, complete with indentation. Will fail on non-JSON-serializable datatypes."""
-		return json.dumps(dct, default=Tools.json_serial, sort_keys=True, indent=4)
-
-	@staticmethod
-	def pp_dict(dct: dict) -> None:
-		"""Pretty-prints a dict to stdout."""
-		print(Tools.pretty_dict(dct))
-
-	@staticmethod
-	def pp_size(obj: object) -> None:
-		"""Prints to stdout a human-readable string of the memory usage of arbitrary Python objects. Ex: 8M for 8 megabytes."""
-		print(_hurrysize(sys.getsizeof(obj)))
 
 	@staticmethod
 	def query(query: peewee.Expression) -> pd.DataFrame:
@@ -45,11 +22,11 @@ class Tools:
 
 	@staticmethod
 	def describe(thing_class: peewee.Model) -> None:
-		Tools.pp_dict(thing_class._meta.columns)
+		pp_dict(thing_class._meta.columns)
 
 	@staticmethod
 	def description(thing_class: peewee.Model) -> None:
-		return pd.DataFrame.from_dict(ast.literal_eval(Tools.pretty_dict(thing_class._meta.columns)), orient='index').rename(columns={0: 'type'})
+		return pd.DataFrame.from_dict(ast.literal_eval(pretty_dict(thing_class._meta.columns)), orient='index').rename(columns={0: 'type'})
 
 	@staticmethod
 	def fetch(thing_class: peewee.Model, thing: Union[any, int, str], name_column: str = 'name'):
@@ -65,7 +42,7 @@ class Tools:
 			raise ValarLookupError("Could not find {} in {}".format(thing, thing_class))
 		return found
 
-	def fetch_to_query(thing_class: peewee.Model, thing: Union[any, int, str], name_column: str = 'name'):
+	def fetch_to_query(thing_class: peewee.Model, thing: Union[any, int, str], name_column: str = 'name') -> List[peewee.Expression]:
 		if isinstance(thing, (int, str, thing_class)):
 			return [thing_class.id == Tools.fetch(thing_class, thing, name_column=name_column).id]
 		elif isinstance(thing, List):
@@ -77,3 +54,4 @@ class Tools:
 
 
 __all__ = ['Tools']
+
