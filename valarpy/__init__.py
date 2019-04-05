2abb83d417616cae99d9a6198885708ec0434d39
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2019-04-04 17:12:49 -0700

    Deleted deprecated code.
    Moved query() to a new ValarpyTools.

--- valarpy/__init__.py
+++ valarpy/__init__.py
@@ -1,3 +1,18 @@
 __version__ = '1.2.0'
 
 valarpy_version = __version__
+
+import peewee
+import pandas as pd
+
+
+class ValarpyTools:
+	@staticmethod
+	def query(query: peewee.Expression) -> pd.DataFrame:
+		return pd.DataFrame([
+			pd.Series(row._data)
+			for row in query
+		])
+
+
+__all__ = ['__version__', 'valarpy_version', 'ValarpyTools']
--- valarpy/utils.py
+++
@@ -1,7 +0,0 @@
-# external code depends on these imports
-from klgists.db.utils import *
-
-# but we might add Valar-specific utils here.
-
-from warnings import warn
-warn("Importing valarpy.utils, which is deprecated", DeprecationWarning)
