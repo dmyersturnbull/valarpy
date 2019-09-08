556719a2b7885edce2aec6e5ccea825497ab0193
Douglas Myers-Turnbull <dmyersturnbull@gmail.com>
2019-09-08 14:19:59 -0700

    1.5.0

--- valarpy/__init__.py
+++ valarpy/__init__.py
@@ -4,7 +4,7 @@ __credits__ = ["Douglas Myers-Turnbull"]
 __license__ = "Apache License, Version 2.0"
 __maintainer__ = "Douglas Myers-Turnbull"
 __status__ = "Production"
-__version__ = "1.4.5"
+__version__ = "1.5.0"
 
 valarpy_version = __version__
 
